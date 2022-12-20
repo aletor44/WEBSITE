a30eabb9af95d0d041667047fc141ca18d8feb33
aletor44 <62729986+aletor44@users.noreply.github.com>
2022-12-19 22:51:36 -0800

    Create bio.php

---
+++ bio.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+<bodys>
+    
+    <div class="logo">BIO</div>       
+        
+    <section class="footballers">
+      <div class="footballers-container">
+        
+        
+        <h1>10. Eli Manning </h1>
+          <p> 
+          Elisha Nelson Manning is a former American football quarterback who played in the National Football League for 16 seasons with the New York Giants.
+          A member of the Manning football dynasty, he is the youngest son of quarterback Archie Manning and younger brother of quarterback Peyton Manning.
+            <img src="https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/5526.png" style="width: 250px;;">
+          </p>
+          <h1>9. Matt Ryan </h1>
+          Matthew Thomas Ryan, nicknamed "Matty Ice", is an American football quarterback for the Indianapolis Colts of the National Football League 
+          <img src="https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/11237.png&w=350&h=254" style="width: 250px;;">
+          </p>
+          <h1>8. Ben Roethlisberger </h1>
+          <p>
+          Benjamin Todd Roethlisberger Sr., nicknamed "Big Ben", is an American former football quarterback who played in the National Football League for
+          18 seasons with the Pittsburgh Steelers. He played college football at Miami and was selected by the Steelers in the first round of the 2004 NFL Draft.
+          <img src="https://static.www.nfl.com/image/private/t_headshot_desktop/league/vziogpnyp9d0xtpiuqhr" style="width: 250px;;">
+          </p>
+          <h1>7. Dan Marino </h1>
+          <p>
+          Daniel Constantine Marino Jr. is an American former football quarterback who played in the National Football League for 17 seasons with the Miami
+          <img src="https://static.www.nfl.com/image/private/t_headshot_desktop/league/jk97vzadmthvdyu8mbr2" style="width: 250px;;">
+          </p>
+          <h1>6. Phillip Rivers </h1>
+          <p>
+          Philip Michael Rivers is an American former football quarterback who played in the National Football League for 17 seasons, primarily with the 
+          Chargers franchise.
+          <img src="https://static.www.nfl.com/image/private/t_headshot_desktop/league/voakyrtj34sghkwxaxxf" style="width: 250px;;">
+          </p>
+          <h1>5. Aaron Rodgers </h1>
+          <p>
+          Aaron Charles Rodgers is an American football quarterback for the Green Bay Packers of the National Football League. 
+          <img src="https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/8439.png" style="width: 250px;;">
+          </p>
+          <h1>4. Brett Favre </h1>
+          <p>
+          Brett Lorenzo Favre is an American former football quarterback who played in the National Football League for 20 seasons, primarily with the Green 
+          Bay Packers. Favre had 321 consecutive starts from 1992 to 2010, including 297 regular season games, the most in league history 
+          <img src="https://o.remove.bg/downloads/1a8f204e-301e-4cd9-b23c-c657609d892f/77323832_original-removebg-preview.png" style="width: 250px;;">
+          </p>
+          <h1>3. Peyton Manning </h1>
+          <p>
+          Peyton Williams Manning is an American former football quarterback who played in the National Football League for 18 seasons. Nicknamed "the Sheriff",
+          he spent 14 seasons with the Indianapolis Colts and four with the Denver Broncos. Manning is considered to be one of the greatest quarterbacks of all time. 
+          <img src="https://static.www.nfl.com/image/private/t_headshot_desktop/league/xpcd6auodk3w0vda6jal" style="width: 250px;;">
+          </p>
+          <h1>2. Drew Brees </h1>
+          <p>
+          Drew Christopher Brees is an American former football quarterback who played in the National Football League for 20 seasons 
+          <img src="https://static.www.nfl.com/image/private/t_headshot_desktop/league/bt6nv11jgsirnuymookt" style="width: 250px;;">
+          </p>
+          <h1>1. Tom Brady </h1>
+          <p>
+          Thomas Edward Patrick Brady Jr. is an American football quarterback for the Tampa Bay Buccaneers of the National Football League. He spent his first 20 
+          seasons with the New England Patriots organization, with which he was a central contributor to the franchise's dynasty from 2001 to 2019. 
+          <img src="https://static.www.nfl.com/image/private/t_headshot_desktop/league/q7dpdlxyu5rs05rgh1le" style="width: 250px;;">
+          </p>
+    <navs>
+<bodys>
