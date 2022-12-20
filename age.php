88b2b18bc840546b522ec1b6c5f0cbe43987fd57
aletor44 <62729986+aletor44@users.noreply.github.com>
2022-12-19 22:50:51 -0800

    Create age.php

---
+++ age.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+<bodys>
+    <div class="logo">AGE</div>  
+
+    <section class="footballers">
+      <div class="footballers-container">
+        
+    <h1>10. Eli Manning </h1>
+          <p>
+          41 years old
+            <img src="https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/5526.png" style="width: 250px;;">
+          </p>
+          <h1>9. Matt Ryan </h1>
+          <p>
+          37 years old 
+          <img src="https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/11237.png&w=350&h=254" style="width: 250px;;">
+          </p>
+          <h1>8. Ben Roethlisberger </h1>
+          <p>
+          40 years old
+          <img src="https://static.www.nfl.com/image/private/t_headshot_desktop/league/vziogpnyp9d0xtpiuqhr" style="width: 250px;;">
+          </p>
+          <h1>7. Dan Marino </h1>
+          <p>
+          61 years old 
+          <img src="https://static.www.nfl.com/image/private/t_headshot_desktop/league/jk97vzadmthvdyu8mbr2" style="width: 250px;;">
+          </p>
+          <h1>6. Phillip Rivers </h1>
+          <p>
+          41 years old 
+          <img src="https://static.www.nfl.com/image/private/t_headshot_desktop/league/voakyrtj34sghkwxaxxf" style="width: 250px;;">
+          </p>
+          <h1>5. Aaron Rodgers </h1>
+          <p>
+          39 years old 
+          <img src="https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/8439.png" style="width: 250px;;">
+          </p>
+          <h1>4. Brett Favre </h1>
+          <p>
+          53 years old 
+          <img src="https://o.remove.bg/downloads/1a8f204e-301e-4cd9-b23c-c657609d892f/77323832_original-removebg-preview.png" style="width: 250px;;">
+          </p>
+          <h1>3. Peyton Manning </h1>
+          <p>
+          46 years old
+          <img src="https://static.www.nfl.com/image/private/t_headshot_desktop/league/xpcd6auodk3w0vda6jal" style="width: 250px;;">
+          </p>
+          <h1>2. Drew Brees </h1>
+          <p>
+          43 years old
+          <img src="https://static.www.nfl.com/image/private/t_headshot_desktop/league/bt6nv11jgsirnuymookt" style="width: 250px;;">
+          </p>
+          <h1>1. Tom Brady </h1>
+          <p>
+          45 years old 
+          <img src="https://static.www.nfl.com/image/private/t_headshot_desktop/league/q7dpdlxyu5rs05rgh1le" style="width: 250px;;">
+          </p>
+    <navs>
+<bodys>
