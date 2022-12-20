3ab8227e1628ead7f8cb8f69892b18ddbee8882b
aletor44 <62729986+aletor44@users.noreply.github.com>
2022-12-19 22:52:40 -0800

    Create writing.php

---
+++ writing.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <nav>
+      <div class="logo">Highest Touchdown Passes NFL History</div>
+      <div class="nav-items">
+        <a href="bio.php">BIO</a>
+         <a href="age.php">AGE</a> 
+      </div>
+
+
+      <div class = "search - box">
+        <input class="search-txt" type= "text" name = "name=" placeholder="Type any football player">
+        <a class ="search-btn" href="#">
+    </nav>
+
+
+    <section class="footballers">
+      <div class="footballers-container">
+        
+      
+          <h1>10. Eli Manning </h1>
+          <p>
+            366 touchdown passes
+            <img src="https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/5526.png" style="width: 250px;;">
+          </p>
+          <h1>9. Matt Ryan </h1>
+          <p>
+          368 touchdown passes 
+          <img src="https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/11237.png&w=350&h=254" style="width: 250px;;">
+          </p>
+          <h1>8. Ben Roethlisberger </h1>
+          <p>
+          418 touchdown passes
+          <img src="https://static.www.nfl.com/image/private/t_headshot_desktop/league/vziogpnyp9d0xtpiuqhr" style="width: 250px;;">
+          </p>
+          <h1>7. Dan Marino </h1>
+          <p>
+          420 touchdown passes 
+          <img src="https://static.www.nfl.com/image/private/t_headshot_desktop/league/jk97vzadmthvdyu8mbr2" style="width: 250px;;">
+          </p>
+          <h1>6. Phillip Rivers </h1>
+          <p>
+          421 touchdown passes 
+          <img src="https://static.www.nfl.com/image/private/t_headshot_desktop/league/voakyrtj34sghkwxaxxf" style="width: 250px;;">
+          </p>
+          <h1>5. Aaron Rodgers </h1>
+          <p>
+          449 touchdown passes 
+          <img src="https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/8439.png" style="width: 250px;;">
+          </p>
+          <h1>4. Brett Favre </h1>
+          <p>
+          508 touchdown passes 
+          <img src="https://o.remove.bg/downloads/1a8f204e-301e-4cd9-b23c-c657609d892f/77323832_original-removebg-preview.png" style="width: 250px;;">
+          </p>
+          <h1>3. Peyton Manning </h1>
+          <p>
+          539 touchdown passes 
+          <img src="https://static.www.nfl.com/image/private/t_headshot_desktop/league/xpcd6auodk3w0vda6jal" style="width: 250px;;">
+          </p>
+          <h1>2. Drew Brees </h1>
+          <p>
+          571 touchdown passes 
+          <img src="https://static.www.nfl.com/image/private/t_headshot_desktop/league/bt6nv11jgsirnuymookt" style="width: 250px;;">
+          </p>
+          <h1>1. Tom Brady </h1>
+          <p>
+          635 touchdown passes 
+          <img src="https://static.www.nfl.com/image/private/t_headshot_desktop/league/q7dpdlxyu5rs05rgh1le" style="width: 250px;;">
+          </p>
+        </div>
+      </div>
+    </section>
+  </body>
+</html>
